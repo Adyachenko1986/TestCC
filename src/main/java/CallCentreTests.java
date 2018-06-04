9636611dd780ab6a74d54b7197c7f5ec722df912
ADyachenko <adyachenko@softrust.ru>
2018-06-04 14:08:03 +0300

    запись на прием + все записи + запись по направлению + вызов врача

--- src/main/java/CallCentreTests.java
+++ src/main/java/CallCentreTests.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,8 +24,7 @@ public class CallCentreTests {
     //авторизация в КЦ
       webDriver.get("http://call.emias.mosreg.ru/cc_dev/to_work/#/");
       System.out.println(webDriver.getCurrentUrl());
-        Thread.sleep(2000);
-
+      Thread.sleep(2000);
       webDriver.findElement(By.id("USER_LOGIN")).click();
       //webDriver.findElement(By.id("USER_LOGIN")).clear();
       webDriver.findElement(By.name("USER_LOGIN")).sendKeys("ccg");
@@ -68,7 +68,7 @@ public class CallCentreTests {
         webDriver.findElement(By.xpath ("//*[@id='ccInfoModal']/div/div/div[3]/button[2]")).click();
 
         //Запись по направлению
-        /*Thread.sleep(5000);
+        Thread.sleep(5000);
         webDriver.findElement(By.xpath ("//*[@id='direction-list-tab']")).click();
         Thread.sleep(2000);
         webDriver.findElement(By.xpath ("//*[@id='direction-list']/div/div[2]/button")).click();
@@ -76,9 +76,33 @@ public class CallCentreTests {
         webDriver.findElement(By.xpath ("//*[@id='a7f391d4-d5d8-44d5-a770-f7b527bb123326f407bc-01c7-4e64-ae16-80ae909b1574']/td[6]/button")).click();
         Thread.sleep(2000);
         webDriver.findElement(By.xpath ("//*[@id='day-doc-schedule']/div/div/div[2]/button[17]")).click();
-        webDriver.findElement(By.xpath ("//*[@id='record-to-doc-form']/div/div[2]/button")).click();*/
+        webDriver.findElement(By.xpath ("//*[@id='record-to-doc-form']/div/div[2]/button")).click();
 
         //Вызов врача
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath ("//div[@class='fast-buttons']//button[text()='Вызов врача']")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.name("snPol")).sendKeys("38-09 560219");
+        webDriver.findElement(By.name("birthday")).sendKeys("30111975");
+        webDriver.findElement(By.xpath ("//button[@id='search-patient']")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.name("address")).sendKeys("адрес");
+        webDriver.findElement(By.name("porch")).sendKeys("38");
+        webDriver.findElement(By.name("floor")).sendKeys("9");
+        webDriver.findElement(By.name("intercomCode")).sendKeys("98");
+        webDriver.findElement(By.name("phone")).sendKeys("1234567890");
+        webDriver.findElement(By.name("complaint")).sendKeys("все болит, ничего не помогает");
+        webDriver.findElement(By.name("phone")).sendKeys("1234567890");
+        webDriver.findElement(By.name("phone")).sendKeys("1234567890");
+        webDriver.findElement(By.xpath ("//*[@id='select2-searchByCities-container']")).click();
+        webDriver.findElement(By.xpath ("//input[@class='select2-search__field']")).sendKeys("Красногорский");
+        webDriver.findElement(By.xpath("//input[@class='select2-search__field']")).sendKeys(Keys.RETURN);
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath ("//button[@title='СТЕНД ЕМИАС МО; Адрес: Московская область, г. Неизвестный, ул. Светлая, д. 5']")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath ("//button[@id='call-doctor-button']")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath ("//*[@id='ccInfoModal']/div/div/div[3]/button")).click();
 
 
 
