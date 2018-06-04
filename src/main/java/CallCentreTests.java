014128599c594bdbfda8e0def4ebf6a3e322a3e7
ADyachenko <adyachenko@softrust.ru>
2018-06-04 11:20:11 +0300

    записи пациента + запись по натправлению

--- src/main/java/CallCentreTests.java
+++ src/main/java/CallCentreTests.java
@@ -43,12 +43,40 @@ public class CallCentreTests {
         webDriver.findElement(By.xpath ("//button[@title='СТЕНД ЕМИАС МО; Адрес: Московская область, г. Неизвестный, ул. Светлая, д. 5']")).click();
         Thread.sleep(2000);
         webDriver.findElement(By.xpath ("//*[contains(text(),'Терапия')]")).click();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         webDriver.findElement(By.xpath ("//*[contains(text(),'Ближайшая запись')]")).click();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         webDriver.findElement(By.xpath ("//*[contains(text(),'13:00')]")).click();
         webDriver.findElement(By.xpath ("//*[contains(text(),'Записать на прием')]")).click();
-        webDriver.findElement(By.xpath ("//*[contains(text(),'')]")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//*[@id='ccInfoModal']/div/div/div[3]/button")).click();
+
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//*[contains(text(),'Все записи пациента')]")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//button[@title='Перенести запись']")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//*[@id='a7f391d4-d5d8-44d5-a770-f7b527bb12330b58bf2f-b6ff-423e-bff8-018953417c50']/td[7]/button")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//*[@id='day-doc-schedule']/div/div/div[2]/button[text()='12:00']")).click();
+        webDriver.findElement(By.xpath ("//*[@id='record-to-doc-form']/div/div[2]/button")).click();
+        webDriver.findElement(By.xpath ("//*[contains(text(),'Записать на прием')]")).click();
+        Thread.sleep(1000);
+
+        webDriver.findElement(By.xpath ("//button[@title='Удалить запись']")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//*[@id='ccInfoModal']/div/div/div[3]/button[text()='Да']")).click();
+
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//*[contains(text(),'Записать по направлению')]")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//*[@id='direction-list']/div/div[2]/button")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//*[@id='a7f391d4-d5d8-44d5-a770-f7b527bb123326f407bc-01c7-4e64-ae16-80ae909b1574']/td[6]/button")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath ("//*[@id='day-doc-schedule']/div/div/div[2]/button[17]")).click();
+        webDriver.findElement(By.xpath ("//*[@id='record-to-doc-form']/div/div[2]/button")).click();
+
 
 
         webDriver.quit();
