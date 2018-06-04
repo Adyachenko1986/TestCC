1dc847d66033953642a3e3f4aa6bf222e0796e45
ADyachenko <adyachenko@softrust.ru>
2018-06-04 16:58:51 +0300

    запись на прием + все записи + запись по направлению + вызов врача

--- src/main/java/CallCentreTests.java
+++ src/main/java/CallCentreTests.java
@@ -22,7 +22,7 @@ public class CallCentreTests {
       WebDriver webDriver = new ChromeDriver();
 
     //авторизация в КЦ
-      webDriver.get("http://call.emias.mosreg.ru/cc_dev/to_work/#/");
+      webDriver.get("http://call.emias.mosreg.ru/call2_dev/to_work/");
       System.out.println(webDriver.getCurrentUrl());
       Thread.sleep(2000);
       webDriver.findElement(By.id("USER_LOGIN")).click();
@@ -33,7 +33,7 @@ public class CallCentreTests {
       webDriver.findElement(By.name("USER_PASSWORD")).sendKeys("ccg123");
       webDriver.findElement(By.xpath ("//button[@type='submit']")).click();
       Thread.sleep(1000);
-      webDriver.get("http://call.emias.mosreg.ru/cc_dev/to_work/");
+      webDriver.get("http://call.emias.mosreg.ru/call2_dev/to_work/");
 
       // Поиск пациента
         webDriver.findElement(By.name("snPol")).sendKeys("38-09 560219");
@@ -42,8 +42,8 @@ public class CallCentreTests {
 
       // Запись на прием
         webDriver.findElement(By.xpath ("//button[@title='СТЕНД ЕМИАС МО; Адрес: Московская область, г. Неизвестный, ул. Светлая, д. 5']")).click();
-        Thread.sleep(2000);
-        webDriver.findElement(By.xpath ("//*[contains(text(),'Терапия')]")).click();
+        Thread.sleep(3000);
+        webDriver.findElement(By.xpath ("//div[@id='spec-doc-list']/div[3]/button")).click();
         Thread.sleep(2000);
         webDriver.findElement(By.xpath ("//*[contains(text(),'Ближайшая запись')]")).click();
         Thread.sleep(2000);
@@ -51,6 +51,20 @@ public class CallCentreTests {
         webDriver.findElement(By.xpath ("//*[contains(text(),'Записать на прием')]")).click();
         Thread.sleep(2000);
         webDriver.findElement(By.xpath ("//*[@id='ccInfoModal']/div/div/div[3]/button")).click();
+        Thread.sleep(2000);
+
+       // Запись в лист ожидания
+        webDriver.findElement(By.xpath ("//button[@title='СТЕНД ЕМИАС МО; Адрес: Московская область, г. Неизвестный, ул. Светлая, д. 5']")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath ("//div[@id='spec-doc-list']/div[4]/button")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath ("//*[contains(text(),'Записать в лист ожидания')]")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath ("//*[@id='complain']")).sendKeys("тест ожидания");
+        webDriver.findElement(By.xpath ("//*[contains(text(),'Оформить')]")).click();
+
+
+
 
        //Все записи пациента
         Thread.sleep(2000);
@@ -58,7 +72,7 @@ public class CallCentreTests {
         Thread.sleep(2000);
         webDriver.findElement(By.xpath ("//button[@title='Перенести запись']")).click();
         Thread.sleep(2000);
-        webDriver.findElement(By.xpath ("//*[@id='a7f391d4-d5d8-44d5-a770-f7b527bb12330b58bf2f-b6ff-423e-bff8-018953417c50']/td[7]/button")).click();
+        webDriver.findElement(By.xpath ("//*[@id='week-doc-schedule-table-body']/tr[2]/td[7]/button")).click();
         Thread.sleep(2000);
         webDriver.findElement(By.xpath ("//*[@id='day-doc-schedule']/div/div/div[2]/button[17]")).click();
         webDriver.findElement(By.xpath ("//*[@id='record-to-doc-form']/div/div[2]/button")).click();
@@ -68,7 +82,7 @@ public class CallCentreTests {
         webDriver.findElement(By.xpath ("//*[@id='ccInfoModal']/div/div/div[3]/button[2]")).click();
 
         //Запись по направлению
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         webDriver.findElement(By.xpath ("//*[@id='direction-list-tab']")).click();
         Thread.sleep(2000);
         webDriver.findElement(By.xpath ("//*[@id='direction-list']/div/div[2]/button")).click();
