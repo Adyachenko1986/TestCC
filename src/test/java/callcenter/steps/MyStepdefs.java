f84075fe8ebf1d6eb118123cbf5c775bc5dfa91f
ADyachenko <adyachenko@softrust.ru>
2018-08-28 18:28:16 +0300

    ...

--- src/test/java/callcenter/steps/MyStepdefs.java
+++ src/test/java/callcenter/steps/MyStepdefs.java
@@ -1,8 +1,17 @@
 package callcenter.steps;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.ru.*;
-import callcenter.AbstractTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pages.Pages;
 import pages.SElements;
 import com.codeborne.selenide.Condition;
 
@@ -13,7 +22,44 @@ import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class MyStepdefs extends AbstractTest implements SElements {
+public class MyStepdefs  implements SElements {
+    public static Pages page;
+    public static WebDriver driver;
+    public static ChromeOptions chromeOptions;
+    public static ChromeDriverService chromeDriverService;
+
+    static String perek = "тест";
+    static String url;
+    static String snpol;
+    static String fam;
+    static String bd;
+    static SelenideElement lpu;
+    static SelenideElement mcod;
+    static SelenideElement apoinmentsList;
+    static SelenideElement apoLpu;
+
+    @Before
+    public static void setupTimeout() {
+        chromeDriverService = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("src/main/resources/chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("window-size=1240,1020");
+        driver = new ChromeDriver(chromeDriverService, chromeOptions);
+        WebDriverRunner.setWebDriver(driver);
+        Configuration.timeout = 8000;
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+//        Configuration.browser = "chrome";
+        page = new Pages();
+    }
+
+    @After
+    public static void afterTests() {
+//        close();
+        driver.quit();
+    }
+
 
     @Допустим("^что Андрей вошел в АРМ оператора под своей учетной записью$")
     public void LoginCC() {
